93c4400d4ad8230b3f0622ac37009ae8568d0e5a
Ty Andrews <ty.elgin.andrews@gmail.com>
2023-03-17 18:05:32 -0700

    bug: path issue on render

--- src/pages/explore_ads.py
+++ src/pages/explore_ads.py
@@ -20,7 +20,9 @@ try:
     SRC_PATH = cur_dir[: cur_dir.index("fortunato-wheels") + len("fortunato-wheels")]
 except ValueError:
     # to be used on render with non-project named directory, it's named src
-    SRC_PATH = cur_dir[: cur_dir.index("src/src") + len("src")]
+    SRC_PATH = cur_dir[
+        : cur_dir.index(os.path.join("src", "src")) + len(os.path.join("src", "src"))
+    ]
 if SRC_PATH not in sys.path:
     sys.path.append(SRC_PATH)
 
